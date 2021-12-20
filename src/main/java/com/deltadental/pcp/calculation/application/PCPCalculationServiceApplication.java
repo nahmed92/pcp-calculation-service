279d34be5169dbc83e25315e2c0c09431db43813
Kishore Basireddy <KBasireddy@delta.org>
2021-12-20 12:42:23 -0500

    pcp calc app

---
+++ src/main/java/com/deltadental/pcp/calculation/application/PCPCalculationServiceApplication.java
@@ -0,0 +1,10 @@
+package com.deltadental.pcp.calculation.application;
+
+public class PCPCalculationServiceApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
