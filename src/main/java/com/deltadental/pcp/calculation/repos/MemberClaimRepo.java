b255d22be9cb8377d13e95b88d887e637cfd59be
Kishore Basireddy <KBasireddy@delta.org>
2022-05-09 16:15:30 -0400

    code refactor

--- src/main/java/com/deltadental/pcp/calculation/repos/MemberClaimRepo.java
+++ src/main/java/com/deltadental/pcp/calculation/repos/MemberClaimRepo.java
@@ -1,9 +1,12 @@
 package com.deltadental.pcp.calculation.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.deltadental.pcp.calculation.entities.MemberClaimEntity;
+
+@Repository
 @Transactional(readOnly = false)
 public interface MemberClaimRepo extends JpaRepository<MemberClaimEntity, Integer> {
 
--- src/main/java/com/deltadental/pcp/calculation/repos/MemberClaimServicesRepo.java
+++ src/main/java/com/deltadental/pcp/calculation/repos/MemberClaimServicesRepo.java
@@ -1,10 +1,12 @@
 package com.deltadental.pcp.calculation.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.deltadental.pcp.calculation.entities.MemberClaimServicesEntity;
 
+@Repository
 @Transactional(readOnly = false)
 public interface MemberClaimServicesRepo extends JpaRepository<MemberClaimServicesEntity, Integer> {
 
