048417ad61ea9bab4440f507064bdd20be9787e0
Kishore Basireddy <KBasireddy@delta.org>
2022-07-12 17:39:56 -0400

    primary key added

--- SQL_Scripts/contract_member_claim_07122022.sql
+++ SQL_Scripts/contract_member_claim_07122022.sql
@@ -14,7 +14,7 @@ CREATE TABLE dbo.contract_member_claim (
 	last_updated_at datetime DEFAULT getdate() NULL,
 	instance_id varchar(25) NULL,
 	error_message varchar(4096) NULL,
-	CONSTRAINT pk_contract_member_claim_id_sequence_id PRIMARY KEY (id,sequence_id)
+	CONSTRAINT pk_contract_member_claim_sequence PRIMARY KEY (id,sequence_id)
 );
 CREATE INDEX idx_instance_id ON dbo.contract_member_claim (  instance_id ASC  ) ;
 CREATE INDEX idx_claim_contract_provider_member_state ON dbo.contract_member_claim (  claim_id ASC  , contract_id ASC  , provider_id ASC  , member_id ASC  , state ASC  );
